9510da56404cf35bb1f89ae4b834f0f7a8ca67de
Kenneth <97321708+Kenneth882@users.noreply.github.com>
2026-09-24 12:55:07 -0400

    Add: sort-array-by-parity - submission-3

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        l=0
+        r=1
+        total=len(nums) 
+        while r<total:
+            if nums[l]%2==0 and nums[r]%2==0:
+                r+=1
+                l+=1
+            elif nums[l]%2==1:
+                
+                while r<total and nums[r]%2==1:
+                    r+=1
+                if r==total:
+                    return nums
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+                r+=1
+            else:
+                l+=1
+                r+=1
+        return nums
+            
+                
